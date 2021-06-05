ca7cda8dfe2b0d9e9bc0558d72d204b89d934748
Apurva_sawant <84436785+apurva2761@users.noreply.github.com>
2021-06-05 13:35:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha We covered
+        everything from Scratch including Javascript React.js, HTML. Do register for Data Analyst and Full stack web dev training & internship by ShapeAI.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
